c74fe49b10996ea150fdaf91a533b88a119a2a27
Shivam Gupta <shivamg.cr7@gmail.com>
2020-07-09 17:29:07 +0530

    [Shivam] Using boto3 to create bucket into aws S3.

---
+++ createS3Bucket.py
@@ -0,0 +1,34 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+
+def create_bucket(bucket_name, region=None):
+    """Create an S3 bucket in a specified region
+
+    If a region is not specified, the bucket is created in the S3 default
+    region (us-east-1).
+
+    :param bucket_name: Bucket to create
+    :param region: String region to create bucket in, e.g., 'us-west-2'
+    :return: True if bucket created, else False
+    """
+
+    # Create bucket
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+            print("Done!")
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name,
+                                    CreateBucketConfiguration=location)
+            print("Done!")
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+create_bucket("googlestockdata")
