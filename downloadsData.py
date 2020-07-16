74cfdb8f823f4a905e41db87f764ee91794f3b5d
Shivam Gupta <shivamg.cr7@gmail.com>
2020-07-16 13:17:51 +0530

    [Shivam] Modify code as function(as oops).

--- downloadsData.py
+++ downloadsData.py
@@ -3,11 +3,16 @@ import sys
 from alpha_vantage.timeseries import TimeSeries
 
 
-# Download stock data
-ticker = str(sys.argv[1])  # take argument from console
-api_key = open('alpha_key.txt').read()
+# ticker = str(sys.argv[1])  # take argument from console
+def dowload_data(ticker, savefilename):
+    '''
+    Download stock price data
+    '''
+    api_key = open('alpha_key.txt').read()
+    ts = TimeSeries(key=api_key, output_format='pandas')
+    data, meta_data = ts.get_intraday(symbol=ticker, interval='1min', outputsize='full')
+    data.to_csv(savefilename)
+    print("Done!")
 
-ts = TimeSeries(key=api_key, output_format='pandas')
-data, meta_data = ts.get_intraday(symbol=ticker, interval='1min', outputsize='full')
-data.to_csv('google_stock_data.csv')
-print("Done!")
+if __name__ == "__main__":
+    dowload_data('GOOGL','google_stock_data.csv')
