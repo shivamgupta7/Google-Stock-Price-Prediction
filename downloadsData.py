6c9ac29d980e556d11cb36489e27be40f55ffbf3
Shivam Gupta <shivamg.cr7@gmail.com>
2020-07-17 20:30:10 +0530

    [Shivam] Modify code access ALPHAVANTAGE_API_KEY from enviorment variable.

--- downloadsData.py
+++ downloadsData.py
@@ -1,14 +1,14 @@
 import pandas
-import sys
+import os
 from alpha_vantage.timeseries import TimeSeries
 
 
-# ticker = str(sys.argv[1])  # take argument from console
 def dowload_data(ticker, savefilename):
     '''
     Download stock price data
     '''
-    api_key = open('alpha_key.txt').read()
+    api_key = os.getenv("ALPHAVANTAGE_API_KEY")
+    # print(api_key)
     ts = TimeSeries(key=api_key, output_format='pandas')
     data, meta_data = ts.get_intraday(symbol=ticker, interval='1min', outputsize='full')
     data.to_csv(savefilename)
